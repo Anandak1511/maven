39278c79b18f66a6cd4782af0a4b6f623b60f084
Anandak1511 <150420299+Anandak1511@users.noreply.github.com>
2023-11-21 12:54:20 +0530

    Update App.java

--- anand ak/src/main/java/ak/App.java
+++ anand ak/src/main/java/ak/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World and good morning what r u doing!" );
+        System.out.println( "Hello World and good morning what r u doing dude!" );
     }
 }
